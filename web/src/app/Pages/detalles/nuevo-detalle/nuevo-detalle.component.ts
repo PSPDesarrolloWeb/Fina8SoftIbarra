13095aee6dfb327192346ddf5eb7349e0687956c
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2023-09-06 17:46:14 -0500

    .

--- web/src/app/Pages/detalles/nuevo-detalle/nuevo-detalle.component.ts
+++ web/src/app/Pages/detalles/nuevo-detalle/nuevo-detalle.component.ts
@@ -21,8 +21,8 @@ export class NuevoDetalleComponent implements OnInit {
 
 
   codigo_pedido: string = '';
-  idPedido: string='';
-  idProducto: string='';
+  idPedido: string = '';
+  idProducto: string = '';
   codigo_producto: string = '';
   cantidad: string = '';
   precio_unidad: string = '';
@@ -39,53 +39,91 @@ export class NuevoDetalleComponent implements OnInit {
   ngOnInit(): void {
 
     this.listarPedido();
-    // this.listarProducto();
+    this.listarProducto();
 
   }
 
   listarPedido() {
     let pedidos = {
-        accion: 'listarPedido',
+      accion: 'listarPedidos',
     };
     this.servicio.postData(pedidos).subscribe(
-        async (res: any) => {
-            if (res.estado == true) {
-                this.pedidos = res.pedidos;
-            } else {
-            }
-        },
-        (error) => {
-            console.log('Error en la conexión');
+      async (res: any) => {
+        if (res.estado == true) {
+          this.pedidos = res.pedidos;
+        } else {
         }
+      },
+      (error) => {
+        console.log('Error en la conexión pedidos');
+      }
     );
   }
 
 
-  // listarProducto() {
-  //   let productos = {
-  //       accion: 'listarProducto',
-  //   };
-  //   this.servicio.postData(productos).subscribe(
-  //       async (res: any) => {
-  //           if (res.estado == true) {
-  //               this.productos = res.productos;
-  //           } else {
-  //           }
-  //       },
-  //       (error) => {
-  //           console.log('Error en la conexión');
-  //       }
-  //   );
-  // }
+  listarProducto() {
+    let productos = {
+      accion: 'listarProductos',
+    };
+    this.servicio.postData(productos).subscribe(
+      async (res: any) => {
+        if (res.estado == true) {
+          this.productos = res.productos;
+        } else {
+        }
+      },
+      (error) => {
+        console.log('Error en la conexión');
+      }
+    );
+  }
 
   onChangePedido(event: any) {
     this.idPedido = event.target.value;
     console.log(this.idPedido)
-}
+  }
 
-onChangeProducto(event: any) {
-  this.idProducto = event.target.value;
-  console.log(this.idProducto)
-}
+  onChangeProducto(event: any) {
+    this.idProducto = event.target.value;
+    console.log(this.idProducto)
+  }
+
+
+
+  addDetalle() {
+    let datos = {
+        accion: 'addDetalle',
+        codigo_pedido: this.idPedido,
+        codigo_producto: this.idProducto,
+        cantidad: this.cantidad,
+        precio_unidad: this.precio_unidad,
+        numero_linea: this.numero_linea,
+        // emprep: this.idPago,
+  
+    }
+  
+    this.servicio.postData(datos).subscribe((res: any) => {
+        if (res.estado == true) {
+            this.toastr.success('<span class="now-ui-icons ui-1_bell-53"></span>Pedido agregado con éxito', '', {
+                timeOut: 8000,
+                closeButton: true,
+                enableHtml: true,
+                toastClass: 'alert alert-success alert-with-icon',
+                positionClass: 'toast-top-right'
+            });
+            this.router.navigateByUrl('/detalles');
+        } else {
+            this.toastr.error('<span class="now-ui-icons ui-1_bell-53"></span>Error al agregar el Pedido', '', {
+                timeOut: 8000,
+                closeButton: true,
+                enableHtml: true,
+                toastClass: 'alert alert-danger alert-with-icon',
+                positionClass: 'toast-top-right'
+            });
+            console.log(res);
+        }
+    });
+  }
+  
 
 }
--- web/src/app/Pages/pedidos/nuevo-pedido/nuevo-pedido.component.ts
+++ web/src/app/Pages/pedidos/nuevo-pedido/nuevo-pedido.component.ts
@@ -25,6 +25,23 @@ export class NuevoPedidoComponent implements OnInit {
 
   comentarios: string = '';
 
+
+
+  pedidos: any = [];
+  productos: any = [];
+
+
+
+
+  codigo_pedido: string = '';
+  idPedido: string='';
+  idProducto: string='';
+  codigo_producto: string = '';
+  cantidad: string = '';
+  precio_unidad: string = '';
+  numero_linea: string = '';
+
+
   constructor(
     private servicio: ApiService,
     private toastr: ToastrService,
@@ -35,6 +52,7 @@ export class NuevoPedidoComponent implements OnInit {
   ngOnInit(): void {
 
     this.listarClientes();
+    this.listarProductos();
   }
 
 
@@ -42,6 +60,13 @@ export class NuevoPedidoComponent implements OnInit {
     this.idCliente = event.target.value;
     console.log(this.idCliente)
 }
+
+onChangeProducto(event: any) {
+  this.idPedido = event.target.value;
+  console.log(this.idPedido)
+}
+
+
   listarClientes() {
     let clientes = {
         accion: 'listarClientes',
@@ -50,6 +75,23 @@ export class NuevoPedidoComponent implements OnInit {
         async (res: any) => {
             if (res.estado == true) {
                 this.clientes = res.clientes;
+            } else {
+            }
+        },
+        (error) => {
+            console.log('Error en la conexión');
+        }
+    );
+  }
+
+  listarProductos() {
+    let productos = {
+        accion: 'listarProductos',
+    };
+    this.servicio.postData(productos).subscribe(
+        async (res: any) => {
+            if (res.estado == true) {
+                this.productos = res.productos;
             } else {
             }
         },
